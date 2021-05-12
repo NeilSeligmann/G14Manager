b8b41af839490c6886d79ae148575f5f9d74834b
Neil Seligmann <1771953+NeilSeligmann@users.noreply.github.com>
2021-05-12 03:06:48 -0300

    Added Keyboard routes

--- web/web.go
+++ web/web.go
@@ -4,17 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/zllovesuki/G14Manager/controller"
+	"github.com/zllovesuki/G14Manager/cxx/plugin/keyboard"
 )
-
-// type HttpServerStruct struct {
-
-// }
-
-// type Notifier struct {
-// 	C    chan util.Notification
-// 	show chan string
-// 	hide chan struct{}
-// }
 
 func NewHttpServer(dep *controller.Dependencies) *gin.Engine {
 	r := gin.Default()
@@ -28,14 +19,93 @@ func NewHttpServer(dep *controller.Dependencies) *gin.Engine {
 		})
 	})
 
-	// r.Run() // listen and serve on 0.0.0.0:8080
+	v1 := r.Group("/v1")
+	{
+		// Keyboard Routes
+		kb := v1.Group("/keyboard")
+		{
+			// kb.GET("/", func(c *gin.Context) {
+			// 	c.JSON(200, gin.H{
+			// 		"message": "Successfully fetched keyboard data",
+			// 		"keyboard": gin.H{
+			// 			"currentBrightness": dep.Keyboard.CurrentBrightness(),
+			// 		},
+			// 	})
+			// })
+
+			kb.POST("/brightness", func(c *gin.Context) {
+				increase := c.Query("increase")
+				decrease := c.Query("decrease")
+				value := c.Query("value")
+
+				if increase == "true" {
+					dep.Keyboard.BrightnessUp()
+				} else if decrease == "true" {
+					dep.Keyboard.BrightnessDown()
+				} else if value != "" {
+					var lvl = keyboard.OFF
+
+					switch value {
+					case "OFF":
+						lvl = keyboard.OFF
+					case "LOW":
+						lvl = keyboard.LOW
+					case "MEDIUM":
+						lvl = keyboard.MEDIUM
+					case "HIGH":
+						lvl = keyboard.HIGH
+					}
+
+					dep.Keyboard.SetBrightness(lvl)
+				} else {
+					c.JSON(400, gin.H{
+						"error": gin.H{
+							"message": "No argument provided. Either 'increase', 'decrease', or 'value' are required",
+						},
+					})
+				}
+
+				c.JSON(200, gin.H{
+					"message": "succcess",
+				})
+			})
+
+			// Enable/Disable touchpad
+			kb.POST("/touchpad", func(c *gin.Context) {
+				dep.Keyboard.ToggleTouchPad()
+
+				c.JSON(200, gin.H{
+					"message": "Successfully toggled touchpad",
+				})
+			})
+
+			type RogKeyBody struct {
+				Actions []string `json:"actions"`
+			}
+
+			// ROG key remap
+			kb.POST("/rog", func(c *gin.Context) {
+				var body RogKeyBody
+
+				if c.ShouldBindJSON(&body) == nil {
+					dep.Keyboard.Config.RogKey = body.Actions
+
+					c.JSON(200, gin.H{
+						"message": "Successfully set new ROG key bindings!",
+					})
+				} else {
+					c.JSON(200, gin.H{
+						"message": "Incorrect body provided!",
+					})
+				}
+			})
+
+		}
+	}
 
 	go func() {
-		print("TEST 1!")
-		r.Run()
-		print("TEST 2!")
+		r.Run() // listen and serve on 0.0.0.0:8080
 	}()
-	print("TEST 123!")
 
 	return r
 }
