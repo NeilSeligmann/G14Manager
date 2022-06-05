61307c910fbf2d8f5352baabaa5a1209a6d82454
Neil Seligmann <git@neilseligmann.com>
2022-06-05 15:30:13 -0300

    fix: permissions denied should now return the client ip

--- web/middleware.go
+++ web/middleware.go
@@ -12,9 +12,9 @@ func IPWhiteList(whitelist map[string]bool) gin.HandlerFunc {
 			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
 				"status":  http.StatusForbidden,
 				"message": "Permission denied, address not in whitelist.",
-				"data": {
-					"clientIp": c.ClientIP()
-				}
+				"data": gin.H{
+					"clientIp": c.ClientIP(),
+				},
 			})
 			return
 		}
